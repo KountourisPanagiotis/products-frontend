5f34705dc1a77d4dc1b5def51cf3c6e8caeb958c
Kountouris Pan <kountourispan022@gmail.com>
2023-04-13 00:33:52 +0300

    products-insert

--- src/app/products/products-insert/products-insert.component.ts
+++ src/app/products/products-insert/products-insert.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Product } from '../products.interfaces';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-products-insert',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./products-insert.component.css']
 })
 export class ProductsInsertComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder, private service: ProductsService) {
+    this.form = this.fb.group({
+      product: ['', [Validators.required, Validators.minLength(3)]],
+      cost: ['', Validators.required],
+      quantity: ['', Validators.required],
+      description: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      const product = this.form.value as Product;
+      this.service.insertProduct(product).subscribe((response) => {
+        console.log(response);
+      });
+    } else {
+      console.log('Form is not valid');
+    }
+  }
 }
